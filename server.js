cdcf30113b33f0e9fbcd9eb9039e2816c1539de6
AnastasiiaHudymenko <106466791+AnastasiiaHudymenko@users.noreply.github.com>
2023-05-12 11:27:29 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const DB_HOST = require("./config");
+const DB_HOST =
+  "mongodb+srv://Anastasia:RBDPUJCnqwkzK5Or@cluster0.kmhxppp.mongodb.net/contacts_64?retryWrites=true&w=majority";
 
 mongoose
   .connect(DB_HOST)
